7d9f07baf943785ebf5662d1a79ca638000d139e
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-22 21:00:13 -0300

    feat: create component Search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,14 @@
 import styles from './styles.module.scss';
-
+import { HiOutlineSearch } from 'react-icons/hi';
 export function Search() {
   return (
     <div className={styles.searchContainer}>
-      <input type="text" />
+      <div className={styles.boxSearch}>
+        <input type='text' placeholder='Digite aqui a sua busca...' />
+        <button type='button'>
+          <HiOutlineSearch />
+        </button>
+      </div>
     </div>
   )
 }
