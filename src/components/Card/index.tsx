e3ff25ba9aba2fe9f4c40659f787e11a7b7f6942
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-26 18:51:02 -0300

    feat: list pokemons in cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,21 +1,58 @@
 import styles from './styles.module.scss';
 import { HiOutlineTrash, HiOutlinePencil } from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import PokemonsService from '../../services/PokemonsService';
+import pokeball from '../../assets/images/poke-ball.png';
+import { PokemonsList } from '../../models/PokemonsList.model';
 
 export function Card() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [pokemonsList, setPokemonsList] = useState<PokemonsList>();
+
+  useEffect(() => {
+    async function loadPokemons() {
+      try {
+        setIsLoading(true);
+  
+        const listPokemons = await PokemonsService.listPokemons();
+        setPokemonsList(listPokemons);
+      } catch {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadPokemons();
+  }, []);
+
   return (
-    <div className={styles.card}>
-      <img src="https://avatars.githubusercontent.com/u/53922139?v=4" alt="image profile" />
-      <p>Lorem ipsum dolor sit amet consectetur</p>
-      <footer>
-        <button>
-          <HiOutlineTrash />
-          Excluir
-        </button>
-        <button>
-          <HiOutlinePencil />
-          Editar
-        </button>
-      </footer>
+    <div className={styles.container}>
+      {isLoading && (
+        <h1>Loading...</h1>
+      )}
+  
+      {hasError && (
+        <h1>Error</h1>
+      )}
+
+      {pokemonsList?.results.map((result) => (
+        <div key={result.name} className={styles.card}>
+          <img src={pokeball} alt={`pokeball ${result.name}`} />
+          <p>{ result.name }</p>
+          <footer>
+            <button type='button' onClick={() => window.alert('Funcionalidade não implementada')}>
+              <HiOutlineTrash />
+              Excluir
+            </button >
+            <button type='button' onClick={() => window.alert('Funcionalidade não implementada')}>
+              <HiOutlinePencil />
+              Editar
+            </button>
+          </footer>
+        </div>
+      ))}
     </div>
   )
 }
