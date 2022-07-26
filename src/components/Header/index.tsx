af7f2c698efa4bcc367c40a88653db5b693a0d4a
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-26 18:51:33 -0300

    refactor: change alt of image logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import styles from './styles.module.scss';
-import logo from '../../assets/images/logo-teste.svg';
+import logo from '../../assets/images/logo-impar.svg';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo Impar" />
     </header>
   )
 }
