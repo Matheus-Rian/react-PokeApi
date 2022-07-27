12136399a3c67ffe687b306f19e69fb14e8d2288
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-26 22:09:22 -0300

    feat: add useSearchPokemon in Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,19 +4,22 @@ import { useEffect, useState } from 'react';
 import PokemonsService from '../../services/PokemonsService';
 import pokeball from '../../assets/images/poke-ball.png';
 import { PokemonsList } from '../../models/PokemonsList.model';
+import { useSearchPokemon } from '../../store';
 
 export function Card() {
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
-  const [pokemonsList, setPokemonsList] = useState<PokemonsList>();
+  const [pokemonsList, setPokemonsList] = useState<PokemonsList[]>();
+  const [pokemonsFilter, setPokemonsFilter] = useState<PokemonsList[]>();
+  const result = useSearchPokemon(s => s.search.result);
 
   useEffect(() => {
     async function loadPokemons() {
       try {
         setIsLoading(true);
-  
+
         const listPokemons = await PokemonsService.listPokemons();
-        setPokemonsList(listPokemons);
+        setPokemonsList(listPokemons.results);
       } catch {
         setHasError(true);
       } finally {
@@ -27,20 +30,34 @@ export function Card() {
     loadPokemons();
   }, []);
 
+  useEffect(() => {
+    if (!result) {
+      setPokemonsFilter(pokemonsList);
+      return;
+    }
+
+    const pokemonsFilter = pokemonsList?.filter(({ name }) => name.toLowerCase().includes(result));
+    setPokemonsFilter(pokemonsFilter);
+  }, [result]);
+
   return (
     <div className={styles.container}>
       {isLoading && (
         <h1>Loading...</h1>
       )}
-  
+
+      {(result && pokemonsFilter?.length === 0) && (
+        <h1>Nenhum pokémon foi encontrado</h1>
+      )}
+
       {hasError && (
         <h1>Error</h1>
       )}
 
-      {pokemonsList?.results.map((result) => (
-        <div key={result.name} className={styles.card}>
-          <img src={pokeball} alt={`pokeball ${result.name}`} />
-          <p>{ result.name }</p>
+      {pokemonsFilter?.map(({ name }) => (
+        <div key={name} className={styles.card}>
+          <img src={pokeball} alt={`pokeball ${name}`} />
+          <p>{name}</p>
           <footer>
             <button type='button' onClick={() => window.alert('Funcionalidade não implementada')}>
               <HiOutlineTrash />
