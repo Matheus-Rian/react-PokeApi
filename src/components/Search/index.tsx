4f0d71ccc5593dab700cf01a1b208c55611cb6b2
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-26 22:12:19 -0300

    feat: add useSearchPokemon in Search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,11 +1,18 @@
 import styles from './styles.module.scss';
 import { HiOutlineSearch } from 'react-icons/hi';
+import { useSearchPokemon } from '../../store';
 
 export function Search() {
+  const handleChangeSearch = useSearchPokemon(s => s.handleChangeSearch);
+
   return (
     <div className={styles.searchContainer}>
       <div className={styles.boxSearch}>
-        <input type='text' placeholder='Digite aqui a sua busca...' />
+        <input 
+          type='text' 
+          placeholder='Digite aqui a sua busca...' 
+          onChange={(ev) => handleChangeSearch(ev.target.value)} 
+        />
         <button type='button'>
           <HiOutlineSearch />
         </button>
